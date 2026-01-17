886c16206a050cd5d854fd478d3ad2db0754e6fd
Luek <luekarherke03@gmail.com>
2026-01-17 12:51:49 +0100

    adding analysis helper to create exposure data for the inclusion exclusion parquet created using the build criteria function ; TODO : think about better, more logic names

--- README.md
+++ README.md
@@ -9,41 +9,25 @@ Inclusion criteria
 
 Exclusion criteria
 - Respiratory acidosis
-- Ketoacidosis
-- Volume Loss > 1,5L
+- Ketoacidosis ( Working definition > 3)
+- Volume Loss > 1,5L ( Currently not implemented )
 - RRT prior to acidosis
-- CKD stages 4, 5 or AKI or GFR<30 at time of acidosis
+- CKD stages 4, 5 or AKI or GFR<30 at time of acidosis ( eGFR probably not implemented since not imputable on the private Charité dataset)
 
 ## Usage 
-To generate the cohort run the bicarb_build_cohort.py file, this should build the cohort in the bicarb_data directory. 
-Import the biuild_analysis_df.py to build the necessary structures for analysis.
+To generate the criteria_parquet using the build_inclusion_exclusion.py file, this should create a DF with all the inclusion criteria and true false values for a given inclusion criteria. 
+TODO : CHANGE TO IMPLEMENT ANALYSIS DF Import the biuild_analysis_df.py to build the necessary structures for analysis.
 Use analyse.py to perform analysis. 
 
 ## Current problems 
-- The SOFA and Lactate inclusion criteria are used at any point in time not 48h prior to inclusion event (acidosis) opposed to the proposed research protocol
-- Ketones do not have a defined value
 - Volume Intake, Outtake function are unclear in inclusion 
-- RRT in diagnosis Frame do not have a Global ICU Stay ID. Imputing them with the Global Hospital Stay ID doesn't include all patients / shouldn't be a problem, since all patients with a ICU stay ID that fall into this case are eliminated.
-- Problem with ReprodICU where there are lots of labs with no patient_info and vice versa. See undefined_labs_problem_analysis.ipynb for details. 
+- Inclusion time is defined on any lab value not on current based on chosen model. 
 
 ## Current Cohort Flowchart
-356,849 Patients in Datasets<br>
--> 8809 <18yo<br>
--> 346,736 with no acidosis<br> 
--> 279 with SOFA <4<br>
--> 99 with Lactate <2<br>
-926 Patiens included<br>
--> 611 with respiratoy acidosis<br>
--> 0 with ketosis<br>
--> 69 with >1.5L Volume loss<br>
--> 0 Patients with prior RRT <br>
--> 0 Patients with CKD, 5 Patiens with eGFR<30<br>
-241 Patients used in analysis <br>
+see inclusion_exclusion ipynb
 
 ## TODO : 
-- cohort size very small after selection of acidosis criteria ? is there an error ?
 - SOFA and eGFG data calculated based on "homemade" model, not the reprodICU standard. (doesn't compile on my computer)
-- Rebuild as original study 
 
 TODO Maxime : 
 - Flowchart 
--- build_inclusion_exclusion_criteria.py
+++ build_inclusion_exclusion_criteria.py
@@ -46,135 +46,139 @@ acidemia_any_lab = ts_labs.filter(
 )
 
 # Incliusion time : first acidemia lab time
-inclusion_time = acidemia_any_lab.group_by("Global ICU Stay ID").agg(pl.col("Time Relative to Admission (seconds)").min().alias("inclusion_time_seconds"))
+inclusion_time_any_lab = acidemia_any_lab.group_by("Global ICU Stay ID").agg(pl.col("Time Relative to Admission (seconds)").min().alias("inclusion_time_seconds_any_lab"))
+inclusion_time_one_lab = acidemia_one_lab.group_by("Global ICU Stay ID").agg(pl.col("Time Relative to Admission (seconds)").min().alias("inclusion_time_seconds_one_lab"))
 
-sofa = sofa_helper.calc_sofa(patient_information, ts_vitals, medications, ts_labs, ts_respiratory)
-sofa = sofa.rename({"id" : "Global ICU Stay ID"})
-# SOFA at any time 
-sofa_at_any_time = sofa.filter(pl.col("sofa") >= 4)
-# SOFA upto inclusion time
-sofa_upto_inclusion_time = sofa.join(inclusion_time, on="Global ICU Stay ID", how="inner").filter(pl.col("sofa") >= 4).filter(
-    pl.col("time") <= pl.col("inclusion_time_seconds")
+def build_criteria_table(inclusion_time, col_name):
+    sofa = sofa_helper.calc_sofa(patient_information, ts_vitals, medications, ts_labs, ts_respiratory)
+    sofa = sofa.rename({"id" : "Global ICU Stay ID"})
+    # SOFA at any time 
+    sofa_at_any_time = sofa.filter(pl.col("sofa") >= 4)
+    # SOFA upto inclusion time
+    sofa_upto_inclusion_time = sofa.join(inclusion_time, on="Global ICU Stay ID", how="inner").filter(pl.col("sofa") >= 4).filter(
+        pl.col("time") <= pl.col(col_name)
+        )
+    # SOFA 48h to inclusion event
+    sofa_48h_to_inclusion = sofa.join(inclusion_time, on="Global ICU Stay ID", how="inner").filter(
+        (pl.col("sofa") >= 4) &
+        (pl.col("time") >= (pl.col(col_name) - 48 * 3600)) &
+        (pl.col("time") <= pl.col(col_name))
     )
-# SOFA 48h to inclusion event
-sofa_48h_to_inclusion = sofa.join(inclusion_time, on="Global ICU Stay ID", how="inner").filter(
-    (pl.col("sofa") >= 4) &
-    (pl.col("time") >= (pl.col("inclusion_time_seconds") - 48 * 3600)) &
-    (pl.col("time") <= pl.col("inclusion_time_seconds"))
-)
 
-# Lactate over 2 at any time
-lactate_any_time = ts_labs.filter(pl.col("Lactate").struct.field("value") >= 2)
-# Lactate upto inclusion time 
-lactate_upto_inclusion = ts_labs.join(inclusion_time, on="Global ICU Stay ID", how="inner").filter(
-    (pl.col("Lactate").struct.field("value") >= 2) &
-    (pl.col("Time Relative to Admission (seconds)") <= pl.col("inclusion_time_seconds"))
-)
-# Lactate 48h to inclusion time
-lactate_48h_to_inclusion = ts_labs.join(inclusion_time, on="Global ICU Stay ID", how="inner").filter(
-    (pl.col("Lactate").struct.field("value") >= 2) &
-    (pl.col("Time Relative to Admission (seconds)") >= (pl.col("inclusion_time_seconds") - 48 * 3600)) &
-    (pl.col("Time Relative to Admission (seconds)") <= pl.col("inclusion_time_seconds"))
-)
-
-####### EXCLUSION CRITERIA ########
-
-# Respiratory acidosis (CO2 >= 45)
-respiratory_acidosis = ts_labs.filter(pl.col("Carbon dioxide").struct.field("value") >= 45)
-
-# Kétoacidosis (Ketones >= 3)
-ketoacidosis = ts_labs.filter(pl.col("Ketones").struct.field("value") >= 3)
-
-## Ignore volume loss for now
-
-# Prior RRT
-## ICD10 codes for RRT
-#5A1D00Z — Performance of urinary filtration, single (standard intermittent hemodialysis)
-#5A1D60Z — Performance of urinary filtration, intermittent
-#5A1D70Z — Performance of urinary filtration, continuous
-#5A1D80Z — Performance of urinary filtration, continuous with replacement fluid
-#5A1D80Z — because ICD-PCS groups these under continuous filtration with replacement fluid.
-#5A1D90Z — Performance of urinary filtration for fluid removal only
-#5A1D90Z — Drainage of peritoneal cavity using percutaneous approach (PD fluid removal)
-#3E1M39Z — Introduction of dialysis solution into peritoneal cavity via percutaneous approach (PD initiation)
-icd10_rrt = ["5A1D00Z", "5A1D60Z", "5A1D70Z", "5A1D80Z", "5A1D90Z", "3E1M39Z"]
-
-## ICD9 codes for RRT
-#39.95 — Hemodialysis (used for CRRT as well)
-#54.98 — Other peritoneal dialysis or ultrafiltration procedure
-#54.98 — Other peritoneal dialysis
-icd9_rrt = ["39.95", "54.98"]
-
-rrt_lf = procedures.filter(
-    pl.col("Procedure ICD Code").is_not_null() &
-    (
-        ((pl.col("Procedure ICD Code Version") == 10) & pl.col("Procedure ICD Code").is_in(icd10_rrt)) |
-        ((pl.col("Procedure ICD Code Version") == 9) & pl.col("Procedure ICD Code").is_in(icd9_rrt)) 
+    # Lactate over 2 at any time
+    lactate_any_time = ts_labs.filter(pl.col("Lactate").struct.field("value") >= 2)
+    # Lactate upto inclusion time 
+    lactate_upto_inclusion = ts_labs.join(inclusion_time, on="Global ICU Stay ID", how="inner").filter(
+        (pl.col("Lactate").struct.field("value") >= 2) &
+        (pl.col("Time Relative to Admission (seconds)") <= pl.col(col_name))
     )
-).join(patient_information.select("Global Person ID", "Global ICU Stay ID"), on="Global Person ID", how="inner").drop("Global ICU Stay ID").rename({"Global ICU Stay ID_right" : "Global ICU Stay ID"})
+    # Lactate 48h to inclusion time
+    lactate_48h_to_inclusion = ts_labs.join(inclusion_time, on="Global ICU Stay ID", how="inner").filter(
+        (pl.col("Lactate").struct.field("value") >= 2) &
+        (pl.col("Time Relative to Admission (seconds)") >= (pl.col(col_name) - 48 * 3600)) &
+        (pl.col("Time Relative to Admission (seconds)") <= pl.col(col_name))
+    )
 
-rrt_at_any_time = rrt_lf.select("Global ICU Stay ID").unique()
-rrt_upto_inclusion = rrt_lf.join(inclusion_time, on="Global ICU Stay ID", how="inner").filter(
-    pl.col("Procedure Start Relative to Admission (seconds)") <= pl.col("inclusion_time_seconds")
-)
+    ####### EXCLUSION CRITERIA ########
 
-# Diagnosed CKD Stage 4 (N18.4, N18.5), AKI (N17.-) or GFR <30 at time of acidosis
-# CKD Stage 4 (N18.4, N18.5), AKI (N17.-)
-icd10_codes = ["N18.4", "N18.5", "N17", "N17.0", "N17.1", "N17.2", "N17.8", "N17.9"]
-icd9_codes = ["585.4", "585.5", "584", "584.5", "584.6", "584.7", "584.8", "584.9"]
+    # Respiratory acidosis (CO2 >= 45)
+    respiratory_acidosis = ts_labs.filter(pl.col("Carbon dioxide").struct.field("value") >= 45)
 
-ckd = diagnoses.filter(
-    ((pl.col("Diagnosis ICD Code Version (source)") == "ICD-9") & pl.col("Diagnosis ICD-9 Code").is_in(icd9_codes)) |
-    ((pl.col("Diagnosis ICD Code Version (source)") == "ICD-10") & pl.col("Diagnosis ICD-10 Code").is_in(icd10_codes))
-).join(
-    inclusion_time, on="Global ICU Stay ID", how="inner"
-).filter(
-    # Either before inclusion time
-    (pl.col("Diagnosis Start Relative to Admission (seconds)") <= pl.col("inclusion_time_seconds"))
-    # OR assuming that if diagnosis time is missing, it was present at admission
-    | pl.col("Diagnosis Start Relative to Admission (seconds)").is_null()
-)
+    # Kétoacidosis (Ketones >= 3)
+    ketoacidosis = ts_labs.filter(pl.col("Ketones").struct.field("value") >= 3)
 
-edfg = edfg_helper.eDFG_ckd_epi(patient_information, ts_labs)
-patients_dfg_before_inclusion = edfg.filter(pl.col("eDFG CKD-EPI") <= 30 ).join(
-    inclusion_time,
-    on="Global ICU Stay ID",
-    how="inner"
-).filter(
-    pl.col("Time Relative to Admission (seconds)") <= pl.col("inclusion_time_seconds")
-)
+    ## Ignore volume loss for now
 
-# Table with columns defining inclusion creteria
-iterable_names_dict = {
-    "include_adults" : adult_patients.select("Global ICU Stay ID").unique(),
-    "include_severe_acidemia_in_48h_one_lab" : acidemia_one_lab.select("Global ICU Stay ID").unique(),
-    "include_severe_acidemia_in_48h_any_lab" : acidemia_any_lab.select("Global ICU Stay ID").unique(),
-    "include_sofa_at_any_time" : sofa_at_any_time.select("Global ICU Stay ID").unique(),
-    "include_sofa_upto_inclusion" : sofa_upto_inclusion_time.select("Global ICU Stay ID").unique(),
-    "include_sofa_48h_to_inclusion" : sofa_48h_to_inclusion.select("Global ICU Stay ID").unique(),
-    "include_lactate_at_any_time" : lactate_any_time.select("Global ICU Stay ID").unique(),
-    "include_lactate_upto_inclusion" : lactate_upto_inclusion.select("Global ICU Stay ID").unique(),
-    "include_lactate_48h_to_inclusion" : lactate_48h_to_inclusion.select("Global ICU Stay ID").unique(),
-    "exclude_respiratory_acidosis" : respiratory_acidosis.select("Global ICU Stay ID").unique(),
-    "exclude_ketoacidosis" : ketoacidosis.select("Global ICU Stay ID").unique(),
-    "exclude_prior_RRT_at_any_time" : rrt_at_any_time.select("Global ICU Stay ID").unique(),
-    "exclude_prior_RRT_upto_inclusion" : rrt_upto_inclusion.select("Global ICU Stay ID").unique(),
-    "exclude_CKD" : ckd.select("Global ICU Stay ID").unique(),
-    "exclude_gfr_below_30" : patients_dfg_before_inclusion.select("Global ICU Stay ID").unique(),
-}
+    # Prior RRT
+    ## ICD10 codes for RRT
+    #5A1D00Z — Performance of urinary filtration, single (standard intermittent hemodialysis)
+    #5A1D60Z — Performance of urinary filtration, intermittent
+    #5A1D70Z — Performance of urinary filtration, continuous
+    #5A1D80Z — Performance of urinary filtration, continuous with replacement fluid
+    #5A1D80Z — because ICD-PCS groups these under continuous filtration with replacement fluid.
+    #5A1D90Z — Performance of urinary filtration for fluid removal only
+    #5A1D90Z — Drainage of peritoneal cavity using percutaneous approach (PD fluid removal)
+    #3E1M39Z — Introduction of dialysis solution into peritoneal cavity via percutaneous approach (PD initiation)
+    icd10_rrt = ["5A1D00Z", "5A1D60Z", "5A1D70Z", "5A1D80Z", "5A1D90Z", "3E1M39Z"]
 
-CRITERIA_TABLE = patient_information.select("Global ICU Stay ID")
-for name, df in iterable_names_dict.items():
-    print(f"Processing criterion: {name}")
-    CRITERIA_TABLE = CRITERIA_TABLE.with_columns([
-        pl.when(pl.col("Global ICU Stay ID").is_in(df.collect().to_series()))
-        .then(pl.lit(True))
-        .otherwise(pl.lit(False))
-        .alias(name)
-    ])
+    ## ICD9 codes for RRT
+    #39.95 — Hemodialysis (used for CRRT as well)
+    #54.98 — Other peritoneal dialysis or ultrafiltration procedure
+    #54.98 — Other peritoneal dialysis
+    icd9_rrt = ["39.95", "54.98"]
 
+    rrt_lf = procedures.filter(
+        pl.col("Procedure ICD Code").is_not_null() &
+        (
+            ((pl.col("Procedure ICD Code Version") == 10) & pl.col("Procedure ICD Code").is_in(icd10_rrt)) |
+            ((pl.col("Procedure ICD Code Version") == 9) & pl.col("Procedure ICD Code").is_in(icd9_rrt)) 
+        )
+    ).join(patient_information.select("Global Person ID", "Global ICU Stay ID"), on="Global Person ID", how="inner").drop("Global ICU Stay ID").rename({"Global ICU Stay ID_right" : "Global ICU Stay ID"})
 
-print(CRITERIA_TABLE.head().collect())
+    rrt_at_any_time = rrt_lf.select("Global ICU Stay ID").unique()
+    rrt_upto_inclusion = rrt_lf.join(inclusion_time, on="Global ICU Stay ID", how="inner").filter(
+        pl.col("Procedure Start Relative to Admission (seconds)") <= pl.col(col_name)
+    )
 
-CRITERIA_TABLE.sink_parquet("inclusion_exclusion_criteria_table.parquet")
+    # Diagnosed CKD Stage 4 (N18.4, N18.5), AKI (N17.-) or GFR <30 at time of acidosis
+    # CKD Stage 4 (N18.4, N18.5), AKI (N17.-)
+    icd10_codes = ["N18.4", "N18.5", "N17", "N17.0", "N17.1", "N17.2", "N17.8", "N17.9"]
+    icd9_codes = ["585.4", "585.5", "584", "584.5", "584.6", "584.7", "584.8", "584.9"]
 
+    ckd = diagnoses.filter(
+        ((pl.col("Diagnosis ICD Code Version (source)") == "ICD-9") & pl.col("Diagnosis ICD-9 Code").is_in(icd9_codes)) |
+        ((pl.col("Diagnosis ICD Code Version (source)") == "ICD-10") & pl.col("Diagnosis ICD-10 Code").is_in(icd10_codes))
+    ).join(
+        inclusion_time, on="Global ICU Stay ID", how="inner"
+    ).filter(
+        # Either before inclusion time
+        (pl.col("Diagnosis Start Relative to Admission (seconds)") <= pl.col(col_name))
+        # OR assuming that if diagnosis time is missing, it was present at admission
+        | pl.col("Diagnosis Start Relative to Admission (seconds)").is_null()
+    )
+
+    edfg = edfg_helper.eDFG_ckd_epi(patient_information, ts_labs)
+    patients_dfg_before_inclusion = edfg.filter(pl.col("eDFG CKD-EPI") <= 30 ).join(
+        inclusion_time,
+        on="Global ICU Stay ID",
+        how="inner"
+    ).filter(
+        pl.col("Time Relative to Admission (seconds)") <= pl.col(col_name)
+    )
+
+    # Table with columns defining inclusion creteria
+    iterable_names_dict = {
+        "include_adults" : adult_patients.select("Global ICU Stay ID").unique(),
+        "include_severe_acidemia_in_48h_one_lab" : acidemia_one_lab.select("Global ICU Stay ID").unique(),
+        "include_severe_acidemia_in_48h_any_lab" : acidemia_any_lab.select("Global ICU Stay ID").unique(),
+        "include_sofa_at_any_time" : sofa_at_any_time.select("Global ICU Stay ID").unique(),
+        "include_sofa_upto_inclusion" : sofa_upto_inclusion_time.select("Global ICU Stay ID").unique(),
+        "include_sofa_48h_to_inclusion" : sofa_48h_to_inclusion.select("Global ICU Stay ID").unique(),
+        "include_lactate_at_any_time" : lactate_any_time.select("Global ICU Stay ID").unique(),
+        "include_lactate_upto_inclusion" : lactate_upto_inclusion.select("Global ICU Stay ID").unique(),
+        "include_lactate_48h_to_inclusion" : lactate_48h_to_inclusion.select("Global ICU Stay ID").unique(),
+        "exclude_respiratory_acidosis" : respiratory_acidosis.select("Global ICU Stay ID").unique(),
+        "exclude_ketoacidosis" : ketoacidosis.select("Global ICU Stay ID").unique(),
+        "exclude_prior_RRT_at_any_time" : rrt_at_any_time.select("Global ICU Stay ID").unique(),
+        "exclude_prior_RRT_upto_inclusion" : rrt_upto_inclusion.select("Global ICU Stay ID").unique(),
+        "exclude_CKD" : ckd.select("Global ICU Stay ID").unique(),
+        "exclude_gfr_below_30" : patients_dfg_before_inclusion.select("Global ICU Stay ID").unique(),
+    }
+
+    CRITERIA_TABLE = patient_information.select("Global ICU Stay ID")
+    for name, df in iterable_names_dict.items():
+        print(f"Processing criterion: {name}")
+        CRITERIA_TABLE = CRITERIA_TABLE.with_columns([
+            pl.when(pl.col("Global ICU Stay ID").is_in(df.collect().to_series()))
+            .then(pl.lit(True))
+            .otherwise(pl.lit(False))
+            .alias(name)
+        ])
+
+    return CRITERIA_TABLE.join(inclusion_time, on="Global ICU Stay ID", how="left")
+
+any_lab_criteria_table = build_criteria_table(inclusion_time_any_lab, "inclusion_time_seconds_any_lab")
+one_lab_criteria_table = build_criteria_table(inclusion_time_one_lab, "inclusion_time_seconds_one_lab")
+
+any_lab_criteria_table.sink_parquet("inclusion_exclusion_criteria_table_any_lab.parquet")
+one_lab_criteria_table.sink_parquet("inclusion_exclusion_criteria_table_one_lab.parquet")
